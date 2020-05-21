54efdbbcd19d0ca27eb5cbe31b2326b037b32055
William Meier <william.meier@outlook.com>
2020-05-20 23:31:59 -0300

    animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,4 +16,39 @@ $(document).ready(function() {
             return false;
         }
     });
+
+    // Animation on scroll
+    $('.js__about').waypoint(function(direction) {
+        $('.js__about').addClass('js__animated animate__fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js__services--down').waypoint(function(direction) {
+        $('.js__services--down').addClass('js__animated animate__fadeInDown');
+    }, {
+        offset: '50%'
+    });
+    $('.js__services--up').waypoint(function(direction) {
+        $('.js__services--up').addClass('js__animated animate__fadeInUp');
+    }, {
+        offset: '80%'
+    });
+
+    $('.js__projects--left').waypoint(function(direction) {
+        $('.js__projects--left').addClass('js__animated animate__fadeInLeft');
+    }, {
+        offset: '60%'
+    });
+    $('.js__projects--right').waypoint(function(direction) {
+        $('.js__projects--right').addClass('js__animated animate__fadeInRight');
+    }, {
+        offset: '60%'
+    });
+
+    $('.js__contact').waypoint(function(direction) {
+        $('.js__contact').addClass('js__animated animate__bounce');
+    }, {
+        offset: '50%'
+    });
 });
